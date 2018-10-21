a86be11ca11f813357dc95a98f32c9112ef9ab6c
yolixtly <yolixtly_13@hotmail.com>
2018-10-21 14:10:32 -0400

    Learn about access modifiers

--- src/class.example.ts
+++ src/class.example.ts
@@ -1,6 +1,6 @@
 export default class User {
     public name: string;
-    public age: number;
+    protected age: number;
 
     constructor(name: string, age?: number) {
         // Define the name and age of the Class instance
@@ -11,6 +11,12 @@ export default class User {
     public sayHi() {
         console.log("Hi, I am: " + this.name + ". I was born in: " + this.calculateBirthYear());
     }
+
+    public updateName(newName) {
+        this.name = newName;
+        console.log("Your name has been updated to: " + newName);
+    }
+
     private calculateBirthYear(): number {
         const currentYear = new Date().getFullYear();
         return currentYear - (this.age);
--- src/index.ts
+++ src/index.ts
@@ -20,13 +20,18 @@ drawPoints({
 import User from "./class.example";
 // We need to alocate memory to this class by creating an instance with the new keyword and the
 // custom information for the instance of the class
+// Constructors are required to alocate data
 const janeDoes = new User("Jane Does", 28);
 // Public Method
 janeDoes.sayHi();
-// Privvate method. Cannot be called
+// Private method. Cannot be called
 // janeDoes.calculateBirthYear();
 
-// Constructors
+// Access modifiers
+janeDoes.updateName("JaneDodiiie");
+// Protected Property cannot be modified after set
+// janeDoes.age = 24;
+
 // Access modifiers
 // Properties
 // Modules
